75f43d309ea2cc1d67b1ab848b478d38094fb02d
Amal M <jrdev@avengersoft.com>
2025-02-10 19:15:54 +0530

    pg-backend

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const path=require('path');
-const userRoutes=require('./routes/user')
-const PostRoutes=require('./routes/post');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const userRoutes = require('./routes/user');
+const PostRoutes = require('./routes/post');
 const bodyParser = require('body-parser');
-mongoose.connect("mongodb+srv://amalsasidharan387:" +  process.env.MONGO_ATLAS_PW + "@cluster0.5caef.mongodb.net/postgram?retryWrites=true&w=majority&appName=Cluster0",
-    {
-        serverSelectionTimeoutMS: 5000, 
-        socketTimeoutMS: 45000, 
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://amalsasidharan387:" + process.env.MONGO_ATLAS_PW + "@cluster0.5caef.mongodb.net/postgram?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
       }
-)
-.then(()=>console.log('connetced to database')).catch((err)=>console.log('Database connection error',err));
+    );
+    console.log('Connected to database');
+  } catch (err) {
+    console.error('Database connection error', err);
+  }
+};
+
+// Initial database connection
+connectToDatabase();
+
+// Reload the database connection every 10 seconds
+setInterval(async () => {
+  try {
+    await mongoose.connection.close();
+    console.log('Database connection closed, reconnecting...');
+    await connectToDatabase();
+  } catch (error) {
+    console.error('Error during database reload', error);
+  }
+}, 60000);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*")
-    res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization")
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    )
-    next()
-})
 
-  
-app.use('/api/Users',userRoutes)
-app.use('/api/Posts',PostRoutes)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
-module.exports=app
+app.use('/api/Users', userRoutes);
+app.use('/api/Posts', PostRoutes);
 
+module.exports = app;
